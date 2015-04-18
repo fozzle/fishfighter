2a99c1e3661b7b3ff967d9ab966f263d27e242eb
Kyle Petrovich <kylepetrovich@gmail.com>
2015-04-18 12:15:57 -0400

    this should work

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private bool jump = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+
+	}
+}
