10c4c15d6e1d349d0559fbd8cad8aa291866e2e7
Kyle Petrovich <kylepetrovich@gmail.com>
2015-04-18 12:46:26 -0400

    hot doo doo

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	private bool jump = false;
+	private Rigidbody2D rigidBody;
+	public float maxSpeed;
+	public float moveForce;
 
 	// Use this for initialization
 	void Start () {
-	
+		rigidBody = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -16,6 +19,21 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void FixedUpdate () {
+		// cache horizontal
+		float h = Input.GetAxis("Horizontal");
 
+		if (h * rigidBody.velocity.x < maxSpeed) {
+			rigidBody.AddForce(Vector2.right * h * moveForce);
+		}
+
+		if (Mathf.Abs(rigidBody.velocity.x) > maxSpeed) {
+			rigidBody.velocity = new Vector2(Mathf.Sign(rigidBody.velocity.x) * maxSpeed, rigidBody.velocity.y);
+		}
+
+		if (jump) {
+			rigidBody.AddForce(Vector2.up * 3);
+
+			jump = false;
+		}
 	}
 }
