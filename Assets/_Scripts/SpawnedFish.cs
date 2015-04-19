7af5331be53a85ab76deda9e83796d38b0e5f30d
byronhulcher <byronhulcher@gmail.com>
2015-04-18 23:12:19 -0400

    Fish spawning! (technically ducks)

---
+++ Assets/_Scripts/SpawnedFish.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnedFish : MonoBehaviour {
+
+	private float speed;
+	private float speedMin = 1f;
+	private float speedRange = 1f;
+	public int direction = 0;
+	// Use this for initialization
+	void Start () {
+		//Debug.Log ("spawned!");
+		speed = ((Random.value * speedRange) + speedMin) * .01f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector2 (transform.position.x + (direction*speed), transform.position.y);
+		if (Mathf.Abs (transform.position.x) > 10f) {
+			DestroyObject(gameObject);
+		}
+	}
+
+	public void setDirection(int newDirection){
+		transform.localScale = new Vector2(newDirection * transform.localScale.x, transform.localScale.y);
+		direction = newDirection;
+		//Debug.Log(newDirection);
+	}
+}
---
+++ Assets/_Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour
+{
+	public float spawnTime = 2f;		// The amount of time between each spawn.
+	public float spawnDelay = 0f;		// The amount of time before spawning starts.
+	
+	public GameObject[] fishOptions;		// Array of enemy prefabs.
+	public Transform[] spawnTransforms;
+
+	void Start ()
+	{
+		// Start calling the Spawn function repeatedly after a delay .
+		InvokeRepeating("Spawn", spawnDelay, spawnTime);
+	}
+	
+	void Spawn()
+	{
+		// Instantiate a random enemy.
+		int enemyIndex = Random.Range(0, fishOptions.Length);
+		int spawnerIndex = Random.Range (0, spawnTransforms.Length);
+
+		GameObject fish = ( GameObject) Instantiate(fishOptions[enemyIndex], new Vector2(spawnTransforms[spawnerIndex].position.x, -1f +(-3f * Random.value)), transform.rotation);
+		SpawnedFish fishScript = fish.GetComponent (typeof(SpawnedFish)) as SpawnedFish ;
+		fishScript.setDirection (spawnerIndex == 0 ? 1 : -1);
+	}
+}
