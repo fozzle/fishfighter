dda5cae96aeddca7692980a2f94a8c07857664d6
byronhulcher <byronhulcher@gmail.com>
2015-04-19 09:38:20 -0400

    Better spawner

--- Assets/_Scripts/SpawnedFish.cs
+++ Assets/_Scripts/SpawnedFish.cs
@@ -3,27 +3,39 @@ using System.Collections;
 
 public class SpawnedFish : MonoBehaviour {
 
-	private float speed;
-	private float speedMin = 1f;
-	private float speedRange = 1f;
+	public float speed;
+	private float speedMax = .4f;
+	private float speedRange = .1f;
 	public int direction = 0;
+	
+
+	private int fishSize;
 	// Use this for initialization
 	void Start () {
-		//Debug.Log ("spawned!");
-		speed = ((Random.value * speedRange) + speedMin) * .01f;
+		setSize (1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.position = new Vector2 (transform.position.x + (direction*speed), transform.position.y);
-		if (Mathf.Abs (transform.position.x) > 10f) {
-			DestroyObject(gameObject);
+		if ((direction == 1 && transform.position.x > 6) || (direction == -1 && transform.position.x < -6 )) {
+			setDirection(direction * -1);
 		}
+		// if (Mathf.Abs (transform.position.x) > 10f) {
+		// 	DestroyObject(gameObject);
+		// }
 	}
 
 	public void setDirection(int newDirection){
-		transform.localScale = new Vector2(newDirection * transform.localScale.x, transform.localScale.y);
+		transform.localScale = new Vector2((float)newDirection * Mathf.Abs(transform.localScale.x), transform.localScale.y);
 		direction = newDirection;
 		//Debug.Log(newDirection);
+	}
+
+	public void setSize(int newSize){
+		speed = (speedMax - ((float)newSize * speedRange) - (.1f * Random.value)) * 0.05f;
+		//Debug.Log (speed);
+		float newScale = .5f + (.3f * (float)newSize);
+		transform.localScale = new Vector2( newScale * transform.localScale.x, newScale * transform.localScale.y);
 	}
 }
--- Assets/_Scripts/Spawner.cs
+++ Assets/_Scripts/Spawner.cs
@@ -8,21 +8,41 @@ public class Spawner : MonoBehaviour
 	
 	public GameObject[] fishOptions;		// Array of enemy prefabs.
 	public Transform[] spawnTransforms;
+	public int[] fishCountBySize;	
 
 	void Start ()
 	{
 		// Start calling the Spawn function repeatedly after a delay .
-		InvokeRepeating("Spawn", spawnDelay, spawnTime);
+		//InvokeRepeating("Spawn", spawnDelay, spawnTime);
+		for (int fishCountIndex = 0; fishCountIndex < fishCountBySize.Length; fishCountIndex++) {
+			for (int spawnCount = 0; spawnCount < fishCountBySize[fishCountIndex]; spawnCount++) {
+				int size = fishCountIndex + 1;
+
+				float range = 12f;
+				float split = range / ((float) (fishCountBySize[fishCountIndex] + 1));
+				int slot = spawnCount + 1;
+				float xPos = ((float)slot * split) + (-.5f * range);
+				Debug.Log (xPos);
+				float yPos = .5f +(-1.25f * size);
+				Vector2 newPosition = new Vector2( xPos, yPos);
+
+
+
+				Spawn (size, newPosition);
+			}
+		}
 	}
 	
-	void Spawn()
+	void Spawn(int size, Vector2 newPosition)
 	{
 		// Instantiate a random enemy.
 		int enemyIndex = Random.Range(0, fishOptions.Length);
 		int spawnerIndex = Random.Range (0, spawnTransforms.Length);
 
-		GameObject fish = ( GameObject) Instantiate(fishOptions[enemyIndex], new Vector2(spawnTransforms[spawnerIndex].position.x, -1f +(-3f * Random.value)), transform.rotation);
+		GameObject fish = ( GameObject) Instantiate(fishOptions[enemyIndex], newPosition, transform.rotation);
 		SpawnedFish fishScript = fish.GetComponent (typeof(SpawnedFish)) as SpawnedFish ;
-		fishScript.setDirection (spawnerIndex == 0 ? 1 : -1);
+		fishScript.setDirection ( (Random.Range(0,2) * 2) - 1 );
+		fishScript.setSize (size);
+
 	}
 }
